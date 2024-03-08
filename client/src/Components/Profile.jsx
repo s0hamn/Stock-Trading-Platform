79fb8198cc9d28d416f35de2d0dbcbdd361774a4
bhushan.shah05 <bhushan.shah05@gmail.com>
2024-03-08 18:21:38 +0530

    adding withdraw and deposit button in profile frontend

--- client/src/Components/Profile.jsx
+++ client/src/Components/Profile.jsx
@@ -25,10 +25,48 @@ const Profile = () => {
         }
     }, [])
 
+    const handleDeposit = () => {
+        alert(user.password);
+        const enteredPassword = prompt('Enter your password to deposit funds: ');
+        let depositAmount = prompt('Enter the amount to deposit: ');
+        depositAmount = Number(depositAmount);
+        
+        // const depositAmount = 12;
+        if(enteredPassword == user.password && !isNaN(depositAmount) && depositAmount > 0 && depositAmount <= 10000 ) {
+            setUser(prevUser => ({
+                ...prevUser,
+                funds: prevUser.funds + depositAmount
+            }));
+            alert('Deposit button clicked');
+        }
+        else{
+            alert(user.password);
+        }
+    };
+
+    const handleWithdraw = () => {
+        alert(user.password);
+        const enteredPassword = prompt('Enter your password to withdraw funds: ');
+        let withdrawAmount = prompt('Enter the amount to withdraw: ');
+        depositAmount = Number(withdrawAmount);
+        
+        // const depositAmount = 12;
+        if(enteredPassword == user.password && !isNaN(withdrawAmount) && withdrawAmount > 0 && withdrawAmount <= 10000 ) {
+
+            setUser(prevUser => ({
+                ...prevUser,
+                funds: prevUser.funds - withdrawAmount
+            }));
+            alert('Deposit button clicked');
+        }
+        else{
+            alert(user.password);
+        }
+    };
     return (
         <>
         <Navbar />
-            <div className="flex flex-col justify-center items-center h-screen bg-gray-200 mt-4 mb-4">
+            <div className="flex flex-col justify-center items-center min-h-screen bg-gray-200 mt-4 mb-4">
                 <h1 className="text-center text-6xl font-bold mb-4 mt-10">My Profile</h1>
                 <div className="grid grid-cols-1 gap-6 bg-white p-10 rounded-lg shadow-md w-4/5 mx-auto">
                     <h2 className="text-center text-4xl font-bold">Personal Information</h2>
@@ -63,6 +101,18 @@ const Profile = () => {
                             <p>{user.funds}</p>
                         </div>
                     </form>
+                </div>
+                <div className="grid grid-cols-1 gap-6 bg-white p-10 rounded-lg shadow-md w-4/5 mx-auto mt-10">
+                    <h2 className="text-center text-4xl font-bold">Available Funds</h2>
+                    <hr />
+                    <div className="flex p-5">
+                        <div className="mb-4 ml-4 mr-40">
+                            <label className="block text-gray-700 text-sm font-bold mb-2">Available funds:</label>
+                            <p>{user.funds}</p>
+                        </div>
+                        <button className="border-2 border-gray-300 px-2 py-1 rounded-lg mr-10" onClick={handleDeposit}>Deposit</button>
+                        <button className="border-2 border-gray-300 p-2 rounded-lg" onClick={handleWithdraw}>Withdraw</button>
+                    </div>
                 </div>
             </div>
         </>
