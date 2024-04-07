bf5b67bf457397135e4b6f737cb3098f17072e0f
SamarthKanwate <samarthkanwate@gmail.com>
2024-04-07 14:05:21 +0530

    changed config url to deployed server url

--- client/vite.config.js
+++ client/vite.config.js
@@ -15,8 +15,8 @@ export default ({ mode }) => {
       proxy: {
         '/api': {
         // Change this to your API server URL
-          // target: process.env.VITE_PROXY_URL, 
-        target: 'http://localhost:3001', 
+          target: process.env.VITE_PROXY_URL, 
+        // target: 'http://localhost:3001', 
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
         },
