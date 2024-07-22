967cb0aa50ab03d44a353819e90da4ab7b1789d7
Soham Naigaonkar <soham.naigaonkar@gmail.com>
2024-07-22 23:09:06 +0530

    Revert "testing"
    This reverts commit 5b5a57afbee12547ffcd3b0394d2b07ad054c78f.

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,6 +11,18 @@ export default ({ mode }) => {
   Object.assign(process.env, env);
 
   return defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          // Change this to your API server URL
+          // target: process.env.VITE_PROXY_URL, 
+          target: 'http://localhost:3001',
+          // target: 'https://stock-trading-platform-o3zp.onrender.com',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
   });
 }
